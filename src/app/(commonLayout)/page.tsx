2fd9f3bac25b23a022111716e65f373013a86d83
shahin2525 <shahin451999@gmail.com>
2025-08-24 13:20:29 +0600

    add navbar

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>wel come to all job bd</h1>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default HomePage;
